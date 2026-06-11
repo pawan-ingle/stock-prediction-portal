1b5d03a3a4ac44ab973f2d3453eb1a94e48ecf4e
Pawan Ingle <pawaningle640@gmail.com>
2026-06-11 12:56:33 +0530

    Homepage React

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
+      <Button />
     </>
   )
 }
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Button from './Button'
+
+
+const Main = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className='text-light lead'>
+                Predict smarter investments with AI-powered stock market analysis. Our platform combines historical data, machine learning models, 
+                and interactive visualizations to deliver accurate stock price forecasts and market insights.
+            </p>
+            <Button text='Login' class="btn-outline-info" />
+            
+
+        </div>
+
+    </div>
+
+    </>
+  )
+}
+
+export default Main
