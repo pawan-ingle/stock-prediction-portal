a31a4e8ba7842535f11ac33242ecf3d4d6c5d980
Pawan Ingle <pawaningle640@gmail.com>
2026-06-15 19:34:26 +0530

    Registration Page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,16 +4,27 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 import Button from './components/Button'
+import Register from './components/Register'
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <BrowserRouter>
       <Header />
-      <Main />
-      <Footer />
-      <Button />
+        <Routes>
+
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />}/>
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
